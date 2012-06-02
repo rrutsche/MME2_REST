733f93059b6d7a869d4a280206d9f81fddfe5f74
Richard Rutsche <richardrutsche@googlemail.com>
2012-06-02 22:15:05 +0200

    changed annotation for the filter ID to IDENTITY

--- ShareYourFilter_REST/src/domain/Filter.java
+++ ShareYourFilter_REST/src/domain/Filter.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -18,7 +19,7 @@ public class Filter implements Serializable {
 	private static final long serialVersionUID = -8665182375873062581L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	private String name;
